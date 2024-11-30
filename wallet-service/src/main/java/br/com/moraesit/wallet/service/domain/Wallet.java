af2bd70e4ed710797510a8b3839722bcd1ed0b42
João Pedro Moraes <moraess.it@gmail.com>
2024-11-30 11:00:09 -0300

    initial commit

---
+++ wallet-service/src/main/java/br/com/moraesit/wallet/service/domain/Wallet.java
@@ -0,0 +1,68 @@
+package br.com.moraesit.wallet.service.domain;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Wallet {
+    private final UUID id;
+    private final WalletType type;
+    private final String fullName;
+    private final String document;
+    private final String email;
+    private final String password;
+    private final BigDecimal balance;
+
+    private Wallet(UUID id, WalletType type, String fullName, String document,
+                   String email, String password, BigDecimal balance) {
+        this.id = id;
+        this.type = type;
+        this.fullName = fullName;
+        this.document = document;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public Wallet create(WalletType type, String fullName, String document, String email, String password) {
+        if (type == null)
+            throw new IllegalArgumentException("type is required");
+        if (StringUtils.isEmpty(fullName))
+            throw new IllegalArgumentException("fullName is required");
+        if (StringUtils.isEmpty(document))
+            throw new IllegalArgumentException("document is required");
+        if (StringUtils.isEmpty(email))
+            throw new IllegalArgumentException("email is required");
+
+        return new Wallet(UUID.randomUUID(), type, fullName, document, email, password, BigDecimal.ZERO);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public WalletType getType() {
+        return type;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+}
